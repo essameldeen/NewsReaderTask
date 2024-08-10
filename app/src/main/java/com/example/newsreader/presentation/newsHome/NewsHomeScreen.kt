573baf2649ba70ae2638f55482a6dda4206b3467
Essam Mohammed <Essam.Mohamed@Mdi.com.eg>
2024-08-11 00:21:58 +0300

    - change icon

--- app/src/main/java/com/example/newsreader/presentation/newsHome/NewsHomeScreen.kt
+++ app/src/main/java/com/example/newsreader/presentation/newsHome/NewsHomeScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
